8372696d7072fcb3f3d5cc0777b245d9470bf310
infzer0 <johnortizo@outlook.com>
2020-02-04 09:44:11 -0500

    Python - Curso - Consulta Tablas Existentes
    Python - Curso: 9 Obtener la Información de las Tablas Existentes en la Base de Datos en Memoria.

--- parte01/1.1_base_datos_memoria.py
+++ parte01/1.1_base_datos_memoria.py
@@ -5,3 +5,19 @@ conexion = sqlite3.connect(':memory:')
 cursor = conexion.cursor()
 
 sql_tabla_estudiante = "CREATE TABLE estudiante(carnet TEXT, nombre TEXT, apelildo TEXT, carrera TEXT, semestre INT, CONSTRAINT carnet_pk PRIMARY KEY (carnet))"
+
+cursor.execute(sql_tabla_estudiante)
+
+sql_consulta_tablas = "SELECT * FROM SQLite_master WHERE type=\"table\""
+
+cursor.execute(sql_consulta_tablas)
+
+print('Tablas disponibles en la base de datos:')
+
+tablas = cursor.fetchall()
+
+for t in tablas:
+    print('Nombre tipo objeto en la BD:', t[0])
+    print('Nombre objeto BD:', t[1])
+    print('Nombre tabla:', t[2])
+    print('Sentencia SQL:', t[4])

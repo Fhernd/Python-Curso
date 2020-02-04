a055632a543d471ed3d51cad9363b4e98c73d097
infzer0 <johnortizo@outlook.com>
2020-02-04 15:11:50 -0500

    Python - Curso - Demostración JSON y Persistencia con píckle
    Python - Curso: 20 Demostración de Parsing con el Formato JSON y Persistencia Datos con pickle.

---
+++ parte01/1.3_parsing_json_persistencia_pickle.py
@@ -0,0 +1,7 @@
+import json
+import pickle
+
+datos = {}
+datos['programadores'] = []
+
+edward = {'id': 1001, 'nombre': 'Edward', 'apellido': 'Ortiz', 'especialidad': 'front-end', 'lenguaje': 'JavaScript', 'email': 'edward@mail.co'}

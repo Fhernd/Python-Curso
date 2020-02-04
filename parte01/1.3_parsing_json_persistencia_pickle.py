5c862511b906d851740fdda0e62018a7963224aa
infzer0 <johnortizo@outlook.com>
2020-02-04 16:09:03 -0500

    Python - Curso - Escritura Archivo JSON
    Python - Curso: 21 Creación de Objetos para un Diccionario y la Creación de un Archivo JSON.

--- parte01/1.3_parsing_json_persistencia_pickle.py
+++ parte01/1.3_parsing_json_persistencia_pickle.py
@@ -5,3 +5,16 @@ datos = {}
 datos['programadores'] = []
 
 edward = {'id': 1001, 'nombre': 'Edward', 'apellido': 'Ortiz', 'especialidad': 'front-end', 'lenguaje': 'JavaScript', 'email': 'edward@mail.co'}
+
+oliva = {'id': 1002, 'nombre': 'Oliva', 'apellido': 'Ordoñez', 'especialidad': 'back-end', 'lenguaje': 'Java', 'email': 'oliva@mail.co'}
+
+juan = {'id': 1003, 'nombre': 'Juan', 'apellido': 'Urbano', 'especialidad': 'front-end', 'lenguaje': 'JavaScript', 'email': 'juan@mail.co'}
+
+datos['programadores'].append(edward)
+datos['programadores'].append(oliva)
+datos['programadores'].append(juan)
+
+print(len(datos['programadores']))
+
+with open('parte01/programadores.json', 'w') as f:
+    json.dump(datos, f)

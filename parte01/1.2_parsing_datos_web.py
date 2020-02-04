dfd0c7a296d2c66c4c011a693f2af2894f9b09d6
infzer0 <johnortizo@outlook.com>
2020-02-04 12:26:16 -0500

    Python - Curso - Consulta del Código HTML
    Python - Curso: 16 Obtener Respuesta del Código HTML del Sitio Web Consultado con requests.

--- parte01/1.2_parsing_datos_web.py
+++ parte01/1.2_parsing_datos_web.py
@@ -4,3 +4,10 @@ import feedparser
 url = 'https://github.com/Fhernd/Python-Curso'
 respuesta = requests.get(url)
 print(respuesta.status_code)
+
+print()
+
+html = respuesta.text
+print(html)
+print()
+print(respuesta.json)

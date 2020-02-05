958675f17a79a691b4e9e1a388497042939fe35f
infzer0 <johnortizo@outlook.com>
2020-02-05 14:18:20 -0500

    Python - Curso - Solución Interactiva Intersección Listas
    Python - Curso: 27 Solución Iterativa para Encontrar los Valores Comunes de Dos Listas.

--- parte02/2.1_listas_comprension.py
+++ parte02/2.1_listas_comprension.py
@@ -22,3 +22,18 @@ pares_triplicados = [n * 3 for n in numeros if n % 2 == 0]
 print(type(pares_triplicados))
 print(len(pares_triplicados))
 print(pares_triplicados)
+
+print()
+
+# Solución con ciclos for (iterativa):
+numeros_1 = [2, 3, 5, 7, 11]
+numeros_2 = [2, 7, 13, 19, 23, 3]
+interseccion = []
+
+for n1 in numeros_1:
+    for n2 in numeros_2:
+        if n1 == n2 and n1 not in interseccion:
+            interseccion.append(n1)
+
+print(len(interseccion))
+print(interseccion)

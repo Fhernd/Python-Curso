09b89246192a6fbe6894e90a65fdc2f2fe80bcb5
infzer0 <johnortizo@outlook.com>
2020-02-05 15:14:40 -0500

    Python - Curso - Conteo Ocurrencias con Counter
    Python - Curso: 30 Conteo de Ocurrencias de Cadenas de Caracteres con la Clase Counter.

--- parte02/2.2_estadistica_counter_numpy.py
+++ parte02/2.2_estadistica_counter_numpy.py
@@ -4,3 +4,20 @@ import numpy as np
 lista_letras = ['t', 'u', 't', 'w', 'x', 'x', 'w', 't', 'y', 'y', 'z']
 contador = Counter(lista_letras)
 print(contador)
+
+print()
+
+dict_letras = {'t': 3, 'u': 1, 'w': 2, 'x': 2, 'y': 2, 'z': 1}
+contador = Counter(dict_letras)
+print(contador)
+
+print()
+
+contador = Counter(t= 3, u=1, w=2, x=2, y=2, z=1)
+print(contador)
+
+print()
+
+cadena = 'tutwxxwtyyz'
+contador = Counter(cadena)
+print(contador)

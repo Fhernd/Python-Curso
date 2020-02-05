911d7ef3d8c780a433049d2d1791a69ae36d1978
infzer0 <johnortizo@outlook.com>
2020-02-05 07:46:52 -0500

    Python - Curso - Inicio Segunda Parte del Curso
    |Python - Curso: 25 Inicio Segunda Parte del Curso - Técnicas de Programación Intermedias y Avanzadas.

---
+++ parte02/2.1_listas_comprension.py
@@ -0,0 +1,17 @@
+numeros = [8, 2, 3, 5, 12, 10, 13, 19, 7, 11, 6, 1]
+
+print(type(numeros))
+print(len(numeros))
+print(numeros)
+
+print()
+
+pares_triplicados = []
+
+for n in numeros:
+    if n % 2 == 0:
+        pares_triplicados.append(n * 3)
+
+print(type(pares_triplicados))
+print(len(pares_triplicados))
+print(pares_triplicados)

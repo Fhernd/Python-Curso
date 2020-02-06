cf9cdf1167a2ed2b482c0fb13c97328406057ff3
infzer0 <johnortizo@outlook.com>
2020-02-06 11:11:51 -0500

    Python - Curso - Definición Funciones para Ejecución Concurrente
    Python - Curso: 43 Definición de las Funciones que se Ejecutarán de Forma Concurrente con gevent.

---
+++ parte04/4.1_programacion_concurrent.py
@@ -0,0 +1,16 @@
+import gevent
+
+def fn1():
+    print('Inicio: fn1')
+    gevent.sleep(0)
+    print('Fin: fn1')
+
+def fn2():
+    print('Inicio: fn2')
+    gevent.sleep(0)
+    print('Fin: fn2')
+
+def fn3():
+    print('Inicio: fn3')
+    gevent.sleep(0)
+    print('Fin: fn3')

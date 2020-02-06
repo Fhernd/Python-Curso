796ad57ff59381ee60786654e83f3a74172616c9
infzer0 <johnortizo@outlook.com>
2020-02-06 12:59:42 -0500

    Python - Curso - Control Ejecución Paralela con Lock
    Python - Curso: 48 Control del Paralelismo de la Ejecución de Funciones con Lock y Process.

--- parte05/5.2_control_ejecucion_paralela.py
+++ parte05/5.2_control_ejecucion_paralela.py
@@ -1,7 +1,7 @@
 import multiprocessing
 
 def retirar(saldo, lock):
-    for i in range(2000):
+    for i in range(500):
         lock.acquire()
         saldo.value = saldo.value - 1
         lock.release()
@@ -12,3 +12,22 @@ def depositar(saldo, lock):
         saldo.value = saldo.value + 1
         lock.release()
 
+def ejecutar_transacciones():
+    saldo = multiprocessing.Value('i', 1000)
+    lock = multiprocessing.Lock()
+
+    proceso_1 = multiprocessing.Process(target=retirar, args=(saldo, lock))
+    proceso_2 = multiprocessing.Process(target=depositar, args=(saldo, lock))
+
+    proceso_1.start()
+    proceso_2.start()
+
+    proceso_1.join()
+    proceso_2.join()
+
+    print('Saldo final: %i' % saldo.value)
+
+
+if __name__ == "__main__":
+    for i in range(10):
+        ejecutar_transacciones()

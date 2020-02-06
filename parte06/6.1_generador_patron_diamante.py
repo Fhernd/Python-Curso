124bcf935ccaa0606ea48cc0cfc370bd3e96f01f
infzer0 <johnortizo@outlook.com>
2020-02-06 16:45:29 -0500

    Python - Curso - Aplicación de Línea de Comandos
    Pytho - Curso: 50 Generación del Patrón Diamante desde una Aplicación de Línea de Comandos (Consola).

--- parte06/6.1_generador_patron_diamante.py
+++ parte06/6.1_generador_patron_diamante.py
@@ -12,4 +12,16 @@ import plac
 #     #
 
 def generador_patron_diamente(filas=6, simbolo='#'):
-    pass
+    "Imprime un patrón tipo diamante a partir de cierta cantidad de filas y un símbolo específico."
+
+    filas = 9
+
+    for i in range(1, filas + 1):
+        i = i - (filas//2 +1)
+        if i < 0:
+            i = -i
+        print(" " * i + simbolo * (filas - i*2) + " "*i)
+    
+if __name__ == "__main__":
+    plac.call(generador_patron_diamente)
+    # plac es una alternativa a argparse

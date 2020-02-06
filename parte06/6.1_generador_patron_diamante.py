c920f7ff7f684e32e9e81d30e4baac5af7b150a3
infzer0 <johnortizo@outlook.com>
2020-02-06 16:08:40 -0500

    Python - Curso - Creación Aplicación de Línea de Comandos
    Python - Curso: 49 Instalación del Módulo plac para la Creación de Aplicaciones Basadas en Consola.

---
+++ parte06/6.1_generador_patron_diamante.py
@@ -0,0 +1,15 @@
+import plac
+# Generador de patrón diamante
+
+#     #
+#    ###
+#   #####
+#  #######
+# #########
+#  #######
+#   #####
+#    ###
+#     #
+
+def generador_patron_diamente(filas=6, simbolo='#'):
+    pass

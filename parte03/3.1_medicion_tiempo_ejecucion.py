7a44b47582e09df55b7b9750bb12623d024dd1d2
infzer0 <johnortizo@outlook.com>
2020-02-06 09:29:59 -0500

    Python - Curso - Comparación Tiempo Ejecución Algoritmos
    Python - Curso: 40 Comparación Tiempo Ejecución Algoritmos de Ordenamiento Selección e Inserción.

--- parte03/3.1_medicion_tiempo_ejecucion.py
+++ parte03/3.1_medicion_tiempo_ejecucion.py
@@ -15,3 +15,35 @@ raices = [lambda i: sqrt(i) for i in range(100)]
 '''
 
 print(timeit(setup=inicializacion, stmt=codigo, number=10000))
+
+print()
+
+# Comparación algoritmos de ordenamiento: Selección vs. Inserción
+
+codigo_seleccion = '''
+A = [64, 25, 12, 22, 11] 
+  
+for i in range(len(A)):
+    min_idx = i
+    for j in range(i+1, len(A)):
+        if A[min_idx] > A[j]:
+            min_idx = j
+              
+    A[i], A[min_idx] = A[min_idx], A[i]
+'''
+
+codigo_insercion = '''
+arr = [64, 25, 12, 22, 11]
+for i in range(1, len(arr)): 
+  
+    key = arr[i] 
+  
+    j = i-1
+    while j >=0 and key < arr[j] : 
+            arr[j+1] = arr[j] 
+            j -= 1
+    arr[j+1] = key
+'''
+
+print(timeit(stmt=codigo_seleccion, number=1000000))
+print(timeit(stmt=codigo_insercion, number=1000000))

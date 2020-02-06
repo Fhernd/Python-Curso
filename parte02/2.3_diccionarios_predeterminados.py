b99ee59e22d1152de0e0556cb94d4e9d462d4d62
infzer0 <johnortizo@outlook.com>
2020-02-06 07:10:38 -0500

    Python - Curso - Conteo Ocurrencias de Cadenas de Caracteres
    Python - Curso: 35 Conteo de Ocurrencias de Cadenas de Caracteres con un Objeto defaultdict.

--- parte02/2.3_diccionarios_predeterminados.py
+++ parte02/2.3_diccionarios_predeterminados.py
@@ -11,3 +11,18 @@ print(versiones_lenguajes['Java'])
 print(versiones_lenguajes['PHP'])
 print(versiones_lenguajes['C#'])
 print(versiones_lenguajes['Python'])
+
+print()
+
+lenguajes = 'Python JavaScript PHP C# Java C++ C PHP PHP Python Python JavaScript'
+lenguajes = lenguajes.split()
+print(lenguajes)
+
+conteo_lenguajes = defaultdict(int)
+for l in lenguajes:
+    conteo_lenguajes[l] += 1
+
+print(conteo_lenguajes)
+print(conteo_lenguajes['Python'])
+print(conteo_lenguajes['C'])
+print(conteo_lenguajes['Kotlin'])

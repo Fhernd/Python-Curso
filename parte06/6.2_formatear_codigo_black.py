3185cd3cba8666605dc86de96c2a1b380e9dd3a7
infzer0 <johnortizo@outlook.com>
2020-02-06 17:07:41 -0500

    Python - Curso - Fin del Curso
    Python - Curso: 51 Formatear un Archivo de Código Python con el Módulo black.

---
+++ parte06/6.2_formatear_codigo_black.py
@@ -0,0 +1,19 @@
+import numpy
+
+numeros = [1, 4, 2, 3]
+
+puntajes = {"Edward": 2, "Daniela": 3, "Oliva": 5}
+
+arreglo = numpy.zeros((2, 6), dtype=numpy.int64)
+
+print(
+    "arreglo = ",
+    arreglo,
+    "\n\n",
+    "list numeros = ",
+    numeros,
+    "\n\n",
+    "puntajes = ",
+    puntajes,
+    "\n",
+)
